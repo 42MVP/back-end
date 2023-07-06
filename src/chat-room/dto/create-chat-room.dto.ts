28fc8d7dd437d818c5d8c769aec38743ee74c7c1
khk811 <hyunkyungkim811@gmail.com>
2023-07-06 15:54:33 +0900

    [BE] 🗑 REMOVE: chat-room 모듈 삭제

--- src/chat-room/dto/create-chat-room.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-import { ChatRoomMode } from 'src/database/entities/enums';
-
-export class CreateChatRoomDto {
-  @IsNotEmpty()
-  roomMode: ChatRoomMode;
-
-  password: string;
-}
--- src/chat-room/dto/update-chat-room.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateChatRoomDto } from './create-chat-room.dto';
-
-export class UpdateChatRoomDto extends PartialType(CreateChatRoomDto) {}
