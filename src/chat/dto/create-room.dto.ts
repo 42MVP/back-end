c8f20e7bec96727ab54e7581530999dd8a1dbb20
khk811 <hyunkyungkim811@gmail.com>
2023-07-21 16:25:58 +0900

    [BE] 📦 ADD: DTO -> Entity 함수를 작성

--- src/chat/dto/create-room.dto.ts
+++ src/chat/dto/create-room.dto.ts
@@ -1,11 +1,21 @@
 import { IsNotEmpty } from 'class-validator';
 import { ChatRoomMode } from '../../database/entities/enums';
+import { ChatRoom } from '../../database/entities/chatroom.entity';
 
-export class CreateChatRoomDto {
+export class CreateRoomDto {
+  @IsNotEmpty()
+  userId: number;
+
+  dmId: number;
+
   roomName: string;
 
   @IsNotEmpty()
   roomMode: ChatRoomMode;
 
   password: string;
+
+  public toChatRoomEntity() {
+    return ChatRoom.from(this.roomName, this.roomMode, this.password);
+  }
 }
