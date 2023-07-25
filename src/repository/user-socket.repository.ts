b5dfc1cb1952152dba300173622df9a311fa8556
khk811 <hyunkyungkim811@gmail.com>
2023-07-25 22:10:05 +0900

    [BE} ♻️ REFACTOR: 주석 삭제, 변수 이름 camel case로 변경

--- src/repository/user-socket.repository.ts
+++ src/repository/user-socket.repository.ts
@@ -3,34 +3,29 @@ import { Injectable } from '@nestjs/common';
 type userId = number;
 type socketId = string;
 
-// interface UserSocket {
-//   userId: number;
-//   socketId: string;
-// }
-
 @Injectable()
 export class UserSocketRepository {
-  private readonly UserSocketMap: Map<userId, socketId> = new Map<userId, socketId>();
+  private readonly userSocketMap: Map<userId, socketId> = new Map<userId, socketId>();
 
   find(userId: userId): socketId | undefined {
-    return this.UserSocketMap.get(userId);
+    return this.userSocketMap.get(userId);
   }
 
   save(userId: userId, socketId: socketId): void {
-    this.UserSocketMap.set(userId, socketId);
+    this.userSocketMap.set(userId, socketId);
     return;
   }
 
   update(userId: userId, socketId: socketId): boolean {
-    if (this.UserSocketMap.get(userId) == undefined) {
+    if (this.userSocketMap.get(userId) == undefined) {
       return false;
     } else {
-      this.UserSocketMap.set(userId, socketId);
+      this.userSocketMap.set(userId, socketId);
       return true;
     }
   }
 
   delete(userId: userId): boolean {
-    return this.UserSocketMap.delete(userId);
+    return this.userSocketMap.delete(userId);
   }
 }
