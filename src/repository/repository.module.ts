c793b9f920acc2f11454c0c7efcf011b56b056e4
khk811 <hyunkyungkim811@gmail.com>
2023-07-25 23:38:26 +0900

    [BE] 📦 ADD: muteTime 저장하는 in-memory repository 추가

--- src/repository/repository.module.ts
+++ src/repository/repository.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserSocketRepository } from './user-socket.repository';
+import { MuteTimeRepository } from './mute-time.repository';
 
 @Module({
-  providers: [UserSocketRepository],
-  exports: [UserSocketRepository],
+  providers: [UserSocketRepository, MuteTimeRepository],
+  exports: [UserSocketRepository, MuteTimeRepository],
 })
 export class RepositoryModule {}
