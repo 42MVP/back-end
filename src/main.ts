18c922a8a6e3bd9a1967aa8c3815508a2061f43b
sinryuji <siler2474@gmail.com>
2023-07-28 19:55:16 +0900

    FIX: reissue 요청시 쿠키 허용을 위한 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,10 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 
