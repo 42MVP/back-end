3cb602991ac91259a4a5de5b8d4884d01feb45c3
kanghyki <kanghyki@gmail.com>
2023-08-10 21:26:35 +0900

    ✨ FEAT: Enter the room when connecting the socket

--- src/connection/connection.gateway.ts
+++ src/connection/connection.gateway.ts
@@ -1,27 +1,40 @@
-import { ConnectedSocket, OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { ConnectionService } from './connection.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { UserSocketRepository } from 'src/repository/user-socket.repository';
 import { AuthService } from 'src/auth/auth.service';
 import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
+@WebSocketGateway({ cors: true })
 export class ConnectionGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(
     private readonly connectionService: ConnectionService,
     private readonly userSocketRepository: UserSocketRepository,
     private readonly authService: AuthService,
   ) {}
+  @WebSocketServer()
+  server: Server;
 
   private readonly logger = new Logger('Gateway');
 
   async handleConnection(@ConnectedSocket() client: Socket) {
     this.logger.log(`${client.id} client is connected`);
     // 개발용
-    const id: number = await this.authService.jwtVerify(client.handshake.headers.authorization);
+    //const id: number = await this.authService.jwtVerify(client.handshake.headers.authorization);
     // 배포용
-    // const id: number = await this.authService.jwtVerify(client.handshake.auth.token);
+    const id: number = await this.authService.jwtVerify(client.handshake.auth.token);
     this.userSocketRepository.save(id, client.id);
+    const userRooms: number[] = await this.connectionService.getUserRoomId(id);
+    console.log('connect room:', userRooms);
+    userRooms.forEach(e => {
+      this.server.in(client.id).socketsJoin(e.toString());
+    });
   }
 
   async handleDisconnect(@ConnectedSocket() client: Socket) {
