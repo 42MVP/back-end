f3366f513734ed7758edb1c83b625a19a60cd087
khk811 <hyunkyungkim811@gmail.com>
2023-08-21 17:44:37 +0900

    :white_check_mark: TEST: dependency error 해결

--- src/temp/temp.module.ts
+++ src/temp/temp.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { GameRepository } from 'src/repository/game.repository';
 import { TempGateway } from './temp.gateway';
+import { RepositoryModule } from 'src/repository/repository.module';
+import { GameService } from 'src/game/game.service';
 import { GameModule } from 'src/game/game.module';
+import { GameGateway } from 'src/game/game.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GameHistory } from 'src/common/entities/game-history.entity';
 
 @Module({
-  imports: [GameModule],
-  providers: [TempGateway, GameRepository],
+  imports: [RepositoryModule, TypeOrmModule.forFeature([GameHistory]), GameModule],
+  providers: [TempGateway, GameService, GameGateway],
 })
 export class TempModule {}
