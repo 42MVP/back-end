ad1c14992a4bf292d76be0fde0a6566bd43834d2
khk811 <hyunkyungkim811@gmail.com>
2023-08-22 17:59:04 +0900

    :zap:  IMPROVE: interface를 class로 전환, matching에 있던 interface 추가

--- src/game/game.interface.ts
+++ src/game/game.interface.ts
@@ -1,3 +1,20 @@
+import { User } from 'src/common/entities/user.entity';
+
+export const defaultSetting: GameSetting = {
+  gameWidth: 1100,
+  gameHeight: 700,
+  matchPoint: 5,
+  paddleWidth: 20,
+  paddleHeight: 100,
+  paddleSpeed: 50,
+  ballRad: 12.5,
+};
+
+const defaultBackground = {
+  gameModeOne: 'black',
+  gameModeTwo: 'pink',
+};
+
 export class Game {
   gameInfo: GameInfo;
   scoreInfo: ScoreInfo;
@@ -6,41 +23,15 @@ export class Game {
   constructor(user1: GameUser, user2: GameUser) {
     this.gameInfo = {
       roomId: 0,
-      leftUser: {
-        userId: user1.userId,
-        userName: user1.userName,
-        userSocket: user1.userSocket,
-      },
-      rightUser: {
-        userId: user2.userId,
-        userName: user2.userName,
-        userSocket: user2.userSocket,
-      },
-      backgroundColor: 'black',
+      leftUser: user1,
+      rightUser: user2,
+      backgroundColor: defaultBackground.gameModeOne,
     };
-    this.scoreInfo = {
-      leftScore: 0,
-      rightScore: 0,
-    };
+    this.scoreInfo = new ScoreInfo();
     this.renderInfo = {
-      leftPaddle: {
-        width: 0,
-        height: 0,
-        x: 0,
-        y: 0,
-      },
-      rightPaddle: {
-        width: 0,
-        height: 0,
-        x: 0,
-        y: 0,
-      },
-      ball: {
-        x: 0,
-        y: 0,
-        dx: 0,
-        dy: 0,
-      },
+      leftPaddle: new Paddle(0, defaultSetting.gameHeight / 2),
+      rightPaddle: new Paddle(defaultSetting.gameHeight - defaultSetting.paddleWidth, defaultSetting.gameHeight / 2),
+      ball: new Ball(),
     };
   }
 }
@@ -52,15 +43,26 @@ export interface GameInfo {
   backgroundColor: string;
 }
 
-export interface GameUser {
+export class GameUser {
   userId: number;
   userName: string;
   userSocket: string;
+
+  constructor(user: User, socketId: string) {
+    this.userId = user.id;
+    this.userName = user.userName;
+    this.userSocket = socketId;
+  }
 }
 
-export interface ScoreInfo {
+export class ScoreInfo {
   leftScore: number;
   rightScore: number;
+
+  constructor() {
+    this.leftScore = 0;
+    this.rightScore = 0;
+  }
 }
 
 export interface RenderInfo {
@@ -69,26 +71,42 @@ export interface RenderInfo {
   ball: Ball;
 }
 
-export interface Paddle {
+export class Paddle {
   width: number;
   height: number;
   x: number;
   y: number;
+
+  constructor(x: number, y: number) {
+    this.width = defaultSetting.paddleWidth;
+    this.height = defaultSetting.paddleHeight;
+    this.x = x;
+    this.y = y;
+  }
 }
 
-export interface Ball {
+export class Ball {
   x: number;
   y: number;
   dx: number;
   dy: number;
+
+  constructor() {
+    this.dx = (Math.random() * 10) % 2 ? 1 : -1;
+    this.dy = (Math.random() * 10) % 2 ? 1 : -1;
+    this.x = defaultSetting.gameWidth / 2;
+    this.y = defaultSetting.gameHeight / 2;
+  }
 }
 
 export interface GameSetting {
-  ballRad: number;
-  paddleSpeed: number;
   gameWidth: number;
   gameHeight: number;
   matchPoint: number;
+  paddleWidth: number;
+  paddleHeight: number;
+  paddleSpeed: number;
+  ballRad: number;
 }
 
 export interface GameResult {
@@ -96,4 +114,44 @@ export interface GameResult {
   defeatId: number;
   winScore: number;
   defeatScore: number;
+}
+
+export interface EmitMatched {
+  matchingId: number;
+  endTimeMs: number;
+}
+
+export class EmitConfirm {
+  result: boolean;
+  leftUser: GameUser | undefined;
+  rightUser: GameUser | undefined;
+  gameRoomId: number | undefined;
+
+  constructor(game: Game | null) {
+    if (game) {
+      this.result = true;
+      this.leftUser = game.gameInfo.leftUser;
+      this.rightUser = game.gameInfo.rightUser;
+      this.gameRoomId = game.gameInfo.roomId;
+    } else {
+      this.result = false;
+      this.leftUser = undefined;
+      this.rightUser = undefined;
+      this.gameRoomId = undefined;
+    }
+  }
+}
+
+export class EmitInit {
+  background: string;
+  leftScore: number;
+  rightScore: number;
+  tableInfo: RenderInfo;
+
+  constructor(game: Game) {
+    this.background = game.gameInfo.backgroundColor;
+    this.leftScore = game.scoreInfo.leftScore;
+    this.rightScore = game.scoreInfo.rightScore;
+    this.tableInfo = game.renderInfo;
+  }
 }
