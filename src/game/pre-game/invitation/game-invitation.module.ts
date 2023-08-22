99e37fbeb9821bfda8df9f840315a94c1489ce54
khk811 <hyunkyungkim811@gmail.com>
2023-08-22 18:03:12 +0900

    :package:  ADD: pre-game module에 game에 필요한 dependency 추가

--- src/game/pre-game/invitation/game-invitation.module.ts
+++ src/game/pre-game/invitation/game-invitation.module.ts
@@ -5,11 +5,14 @@ import { RepositoryModule } from 'src/repository/repository.module';
 import { GameInvitationController } from './game-invitation.controller';
 import { GameInvitationGateway } from './game-invitation.gateway';
 import { GameInvitationService } from './game-invitation.service';
+import { GameHistory } from 'src/common/entities/game-history.entity';
+import { GameGateway } from 'src/game/game.gateway';
+import { GameService } from 'src/game/game.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), RepositoryModule],
+  imports: [TypeOrmModule.forFeature([User, GameHistory]), RepositoryModule],
   controllers: [GameInvitationController],
-  providers: [GameInvitationService, GameInvitationGateway],
+  providers: [GameInvitationService, GameInvitationGateway, GameGateway, GameService],
   exports: [GameInvitationGateway],
 })
 export class GameInvitationModule {}
