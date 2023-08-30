7c1cf38ea98b0c824269538c3ffc04a2fb9da2ce
sinryuji <siler2474@gmail.com>
2023-08-30 18:32:32 +0900

    refactor: user register default image

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { AuthService } from '../auth/auth.service';
 import { User } from '../common/entities/user.entity';
 import { UserService } from '../user/user.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class LoginService {
-  constructor(private readonly userService: UserService, private readonly authService: AuthService) {}
+  constructor(private readonly userService: UserService, private readonly configService: ConfigService) {}
 
   async register(user: User): Promise<number> {
     user.userName = user.intraId;
+    user.avatar = this.configService.get('AWS_S3_DEFAULT_AVATAR');
     return (await this.userService.create(user)).id;
   }
 }
